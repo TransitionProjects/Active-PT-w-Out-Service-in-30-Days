f3b1d521ce4f17f6ef8b282fe4a6df551ef268a7
TPI Marienburg <david.katz@tprojects.org>
2018-12-11 07:50:04 -0800

    Create activitycheck.py

---
+++ activitycheck.py
@@ -0,0 +1,8 @@
+__author__ = "David Marienburg"
+__version__ = ".1"
+
+"""
+This script will, using pandas and the datetime library, check to see if
+participants with an open entry into the Support Services Employment Department
+have had a service in the last 30 days.  Those who have not will be flagged.
+"""
